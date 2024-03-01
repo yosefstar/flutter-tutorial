4edb26bad73c09008aa79052e8510af08f711820
タオル <yoseimine0528@gmail.com>
2024-03-01 16:10:03 +0900

    indexページから、youtubeページに遷移できるように実装。

---
+++ flutter_tutorial/lib/index_page.dart
@@ -0,0 +1,26 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+// Project imports:
+import 'package:flutter_tutorial/youtube/youtube_page.dart';
+
+class IndexPage extends StatelessWidget {
+  const IndexPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const YoutubePage()),
+            );
+          },
+          child: const Text('Youtube'),
+        ),
+      ),
+    );
+  }
+}
---
+++ flutter_tutorial/lib/youtube/youtube_page.dart
@@ -0,0 +1,15 @@
+// Flutter imports:
+import 'package:flutter/material.dart';
+
+class YoutubePage extends StatelessWidget {
+  const YoutubePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('Hello Youtube'),
+      ),
+    );
+  }
+}
