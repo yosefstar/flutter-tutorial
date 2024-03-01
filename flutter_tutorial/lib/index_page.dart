db8b2b998f5d6f21be236b4225c7e79f9f41d60a
タオル <yoseimine0528@gmail.com>
2024-03-01 15:02:07 +0900

    index_pageに、AnimationPageに遷移するボタンを追加

--- flutter_tutorial/lib/index_page.dart
+++ flutter_tutorial/lib/index_page.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/material.dart';
 
 // Project imports:
+import 'package:flutter_tutorial/animation/animation_page.dart';
 import 'package:flutter_tutorial/building/building_layout_page.dart';
 
 class IndexPage extends StatelessWidget {
@@ -11,15 +12,30 @@ class IndexPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => const BuildingLayoutPage()),
-            );
-          },
-          child: const Text('Tutorial1'),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BuildingLayoutPage()),
+                );
+              },
+              child: const Text('Tutorial1'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AnimationPage()),
+                );
+              },
+              child: const Text('Tutorial1-1'),
+            ),
+          ],
         ),
       ),
     );
