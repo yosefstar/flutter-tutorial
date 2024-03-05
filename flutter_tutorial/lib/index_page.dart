747ba3d605e30c5c4cffc902c262c7c83361fc09
タオル <yoseimine0528@gmail.com>
2024-03-05 15:15:03 +0900

    indexページからResidencePageの遷移を実装

--- flutter_tutorial/lib/index_page.dart
+++ flutter_tutorial/lib/index_page.dart
@@ -1,5 +1,6 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/residence/residence_page.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({Key? key}) : super(key: key);
@@ -7,10 +8,18 @@ class IndexPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
       body: Center(
         child: ElevatedButton(
-          onPressed: () {},
-          child: const Text('Tutorial1'),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(builder: (context) => const ResidencePage()),
+            );
+          },
+          child: const Text('Residence'),
         ),
       ),
     );
