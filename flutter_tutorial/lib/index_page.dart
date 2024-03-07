632d37412a7abc52c9788d10caa040a63a2c0dca
タオル <yoseimine0528@gmail.com>
2024-03-07 11:42:11 +0900

    コンフリクトの解消と、indexページの編集。

--- flutter_tutorial/lib/index_page.dart
+++ flutter_tutorial/lib/index_page.dart
@@ -1,6 +1,8 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/building/building_layout_page.dart';
 import 'package:flutter_tutorial/residence/residence_page.dart';
+import 'package:flutter_tutorial/youtube/youtube_page.dart';
 
 class IndexPage extends StatelessWidget {
   const IndexPage({Key? key}) : super(key: key);
@@ -12,14 +14,38 @@ class IndexPage extends StatelessWidget {
         title: const Text('Home'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const ResidencePage()),
-            );
-          },
-          child: const Text('Residence'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BuildingLayoutPage()),
+                );
+              },
+              child: const Text('BuildingLayout'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const YoutubePage()),
+                );
+              },
+              child: const Text('Youtube'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const ResidencePage()),
+                );
+              },
+              child: const Text('Residence'),
+            ),
+          ],
         ),
       ),
     );
