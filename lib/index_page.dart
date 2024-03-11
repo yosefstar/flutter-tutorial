a6e5c84ccd9beee5cb88b59629bbe7222da94418
タオル <yoseimine0528@gmail.com>
2024-03-11 10:28:49 +0900

    Animationページを作成。 indexページを整理。

--- lib/index_page.dart
+++ lib/index_page.dart
@@ -1,5 +1,7 @@
 // Flutter imports:
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorial/animation/animation_page.dart';
+import 'package:flutter_tutorial/building/building_layout_page.dart';
 
 // Project imports:
 import 'package:flutter_tutorial/youtube/youtube_page.dart';
@@ -11,14 +13,38 @@ class IndexPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const YoutubePage()),
-            );
-          },
-          child: const Text('Youtube'),
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const BuildingLayoutPage()),
+                );
+              },
+              child: const Text('BuildingLayout'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const AnimationPage()),
+                );
+              },
+              child: const Text('Animation'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const YoutubePage()),
+                );
+              },
+              child: const Text('Youtube'),
+            ),
+          ],
         ),
       ),
     );
