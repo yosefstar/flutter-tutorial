c5b9856037271d592ae29f4d439e6003b807c0b0
タオル <yoseimine0528@gmail.com>
2024-03-12 17:10:20 +0900

    リファクタリング。FutureBuilderを使用。

--- lib/async/async_page.dart
+++ lib/async/async_page.dart
@@ -9,49 +9,39 @@ class AsyncPage extends StatefulWidget {
 }
 
 class AsyncPageState extends State<AsyncPage> {
-  String _name = '未設定';
-  String _age = '未設定';
-  String _birthday = '未設定';
   final _userInfoStorage = _UserInfoStorage();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadUserInfo();
-  }
-
-  Future<void> _loadUserInfo() async {
-    final userInfo = await _userInfoStorage.loadUserInfo();
-    setState(() {
-      _name = userInfo['name']!;
-      _age = userInfo['age']!;
-      _birthday = userInfo['birthday']!;
-    });
-  }
-
-  void _showEditDialog() {
-    showEditDialog(context, _name, _age, _birthday, (name, age, birthday) {
-      _userInfoStorage
-          .saveUserInfo(name, age, birthday)
-          .then((_) => _loadUserInfo());
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text('名前: $_name'),
-            Text('年齢: $_age'),
-            Text('誕生日: $_birthday'),
-          ],
-        ),
+      body: FutureBuilder<Map<String, String>>(
+        future: _userInfoStorage.loadUserInfo(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return const Center(child: Text('エラーが発生しました'));
+          } else {
+            final userInfo = snapshot.data!;
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text('名前: ${userInfo['name']}'),
+                  Text('年齢: ${userInfo['age']}'),
+                  Text('誕生日: ${userInfo['birthday']}'),
+                ],
+              ),
+            );
+          }
+        },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _showEditDialog,
+        onPressed: () {
+          _userInfoStorage.loadUserInfo().then((userInfo) {
+            _showEditDialog(context, userInfo);
+          });
+        },
         tooltip: '編集',
         backgroundColor: Colors.green,
         shape: const CircleBorder(),
@@ -60,6 +50,83 @@ class AsyncPageState extends State<AsyncPage> {
           color: Colors.white,
         ),
       ),
+    );
+  }
+
+  void _showEditDialog(BuildContext context, Map<String, String> userInfo) {
+    final nameController = TextEditingController(text: userInfo['name']);
+    final ageController = TextEditingController(text: userInfo['age']);
+    final birthdayController =
+        TextEditingController(text: userInfo['birthday']);
+
+    showDialog<void>(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('登録'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              UserInfoTextField(
+                labelText: '名前',
+                controller: nameController,
+              ),
+              UserInfoTextField(
+                labelText: '年齢',
+                controller: ageController,
+                keyboardType: TextInputType.number,
+              ),
+              UserInfoTextField(
+                labelText: '誕生日',
+                controller: birthdayController,
+              ),
+            ],
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.of(context).pop(),
+              child: const Text('キャンセル'),
+            ),
+            TextButton(
+              onPressed: () {
+                _userInfoStorage
+                    .saveUserInfo(
+                  nameController.text,
+                  ageController.text,
+                  birthdayController.text,
+                )
+                    .then((_) {
+                  Navigator.of(context).pop();
+                  setState(() {});
+                });
+              },
+              child: const Text('保存'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
+
+class UserInfoTextField extends StatelessWidget {
+  const UserInfoTextField({
+    super.key,
+    required this.labelText,
+    required this.controller,
+    this.keyboardType = TextInputType.text,
+  });
+  final String labelText;
+  final TextEditingController controller;
+  final TextInputType keyboardType;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(labelText: labelText),
+      cursorColor: Colors.blue,
+      controller: controller,
+      keyboardType: keyboardType,
     );
   }
 }
@@ -79,103 +146,5 @@ class _UserInfoStorage {
     await prefs.setString('name', name);
     await prefs.setString('age', age);
     await prefs.setString('birthday', birthday);
-  }
-}
-
-void showEditDialog(
-  BuildContext context,
-  String name,
-  String age,
-  String birthday,
-  void Function(String, String, String) onSave,
-) {
-  showDialog<void>(
-    context: context,
-    builder: (context) => _EditDialog(
-      name: name,
-      age: age,
-      birthday: birthday,
-      onSave: onSave,
-    ),
-  );
-}
-
-class _EditDialog extends StatelessWidget {
-  const _EditDialog({
-    required this.name,
-    required this.age,
-    required this.birthday,
-    required this.onSave,
-  });
-  final String name;
-  final String age;
-  final String birthday;
-  final void Function(String, String, String) onSave;
-
-  @override
-  Widget build(BuildContext context) {
-    final nameController = TextEditingController(text: name);
-    final ageController = TextEditingController(text: age);
-    final birthdayController = TextEditingController(text: birthday);
-
-    return AlertDialog(
-      title: const Text('登録'),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          TextField(
-            decoration: const InputDecoration(labelText: '名前'),
-            controller: nameController,
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: '年齢'),
-            controller: ageController,
-            keyboardType: TextInputType.number,
-          ),
-          TextField(
-            decoration: const InputDecoration(labelText: '誕生日'),
-            controller: birthdayController,
-          ),
-        ],
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.of(context).pop(),
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.blue),
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
-              ),
-            ),
-          ),
-          child: const Text('Cancel'),
-        ),
-        TextButton(
-          onPressed: () {
-            onSave(
-              nameController.text,
-              ageController.text,
-              birthdayController.text,
-            );
-            Navigator.of(context).pop();
-          },
-          style: ButtonStyle(
-            backgroundColor: MaterialStateProperty.all(Colors.blue),
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-            shape: MaterialStateProperty.all(
-              RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(4),
-              ),
-            ),
-          ),
-          child: const Text('保存'),
-        ),
-      ],
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(4),
-      ),
-    );
   }
 }
