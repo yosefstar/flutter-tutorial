48ede81df187f4ee9c0fbcb3139f1636cee27fc3
タオル <yoseimine0528@gmail.com>
2024-03-26 11:39:24 +0900

    コンフリクトの解消

--- lib/youtube/ui/youtube_page.dart
+++ lib/youtube/ui/youtube_page.dart
@@ -17,7 +17,7 @@ class YoutubePage extends ConsumerWidget {
       loading: () =>
           const Scaffold(body: Center(child: CircularProgressIndicator())),
       error: (error, stack) => _createErrorUI(error),
-      data: (videoInfos) => _createDataUI(videoInfos),
+      data: _createDataUI,
     );
   }
 
@@ -69,11 +69,11 @@ class YoutubePage extends ConsumerWidget {
 }
 
 class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const _CustomAppBar({Key? key}) : super(key: key);
+  const _CustomAppBar();
 
   @override
   Widget build(BuildContext context) {
-    const String appTitle = 'Youtube';
+    const appTitle = 'Youtube';
     return AppBar(
       automaticallyImplyLeading: false,
       title: Row(
@@ -88,8 +88,13 @@ class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         _CustomIconButton(iconData: Icons.notifications_none, onPressed: () {}),
         _CustomIconButton(iconData: Icons.search, onPressed: () {}),
         ClipOval(
-            child: Image.asset('images/icon_user.png',
-                width: 30, height: 30, fit: BoxFit.cover)),
+          child: Image.asset(
+            'images/icon_user.png',
+            width: 30,
+            height: 30,
+            fit: BoxFit.cover,
+          ),
+        ),
         const SizedBox(width: 16),
       ],
     );
@@ -100,37 +105,35 @@ class _CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
 }
 
 class _VideosCardsSection extends StatelessWidget {
-  const _VideosCardsSection({Key? key}) : super(key: key);
+  const _VideosCardsSection();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(12.0),
-      height: 240.0,
+      padding: const EdgeInsets.all(12),
+      height: 240,
       child: _VideosCardsGridView(cardItems: _cardItems),
     );
   }
 }
 
 class _VideosCardsGridView extends StatelessWidget {
+  const _VideosCardsGridView({required this.cardItems});
   final List<_VideosCardsItem> cardItems;
-
-  const _VideosCardsGridView({Key? key, required this.cardItems})
-      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GridView.count(
       crossAxisCount: 2, // 横に2つのカード
-      crossAxisSpacing: 4.0,
-      mainAxisSpacing: 4.0,
+      crossAxisSpacing: 4,
+      mainAxisSpacing: 4,
       childAspectRatio: 4 / 1,
       children: List.generate(cardItems.length, (index) {
         return Center(
           child: Card(
             color: cardItems[index].color,
             shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(4.0), // 角の丸みを設定
+              borderRadius: BorderRadius.circular(4), // 角の丸みを設定
             ),
             child: SizedBox(
               height: 50,
@@ -161,12 +164,14 @@ class _VideosCardsGridView extends StatelessWidget {
 }
 
 class _VideosCardsItem {
+  _VideosCardsItem({
+    required this.iconData,
+    required this.text,
+    required this.color,
+  });
   final IconData iconData;
   final String text;
   final Color color;
-
-  _VideosCardsItem(
-      {required this.iconData, required this.text, required this.color});
 }
 
 class _CustomColors {
@@ -182,33 +187,51 @@ class _CustomColors {
 
 final List<_VideosCardsItem> _cardItems = [
   _VideosCardsItem(
-      iconData: Icons.local_fire_department,
-      text: '急上昇',
-      color: _CustomColors.trending),
+    iconData: Icons.local_fire_department,
+    text: '急上昇',
+    color: _CustomColors.trending,
+  ),
   _VideosCardsItem(
-      iconData: Icons.music_note, text: '音楽', color: _CustomColors.music),
+    iconData: Icons.music_note,
+    text: '音楽',
+    color: _CustomColors.music,
+  ),
   _VideosCardsItem(
-      iconData: Icons.games, text: 'ゲーム', color: _CustomColors.games),
+    iconData: Icons.games,
+    text: 'ゲーム',
+    color: _CustomColors.games,
+  ),
   _VideosCardsItem(
-      iconData: Icons.article, text: 'ニュース', color: _CustomColors.news),
+    iconData: Icons.article,
+    text: 'ニュース',
+    color: _CustomColors.news,
+  ),
   _VideosCardsItem(
-      iconData: Icons.highlight, text: '学び', color: _CustomColors.learning),
+    iconData: Icons.highlight,
+    text: '学び',
+    color: _CustomColors.learning,
+  ),
   _VideosCardsItem(
-      iconData: Icons.live_tv, text: 'ライブ', color: _CustomColors.live),
+    iconData: Icons.live_tv,
+    text: 'ライブ',
+    color: _CustomColors.live,
+  ),
   _VideosCardsItem(
-      iconData: Icons.sports, text: 'スポーツ', color: _CustomColors.sports),
+    iconData: Icons.sports,
+    text: 'スポーツ',
+    color: _CustomColors.sports,
+  ),
 ];
 
 class _VideosHeader extends StatelessWidget {
-  const _VideosHeader({Key? key}) : super(key: key);
+  const _VideosHeader();
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 56.0,
+      height: 56,
       color: _CustomColors.darkGrey,
       child: const Row(
-        mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           SizedBox(width: 20),
           Text(
@@ -226,13 +249,12 @@ class _VideosHeader extends StatelessWidget {
 }
 
 class _VideoList extends StatelessWidget {
+  const _VideoList({required this.videoInfo});
   final VideoInfo videoInfo;
-  const _VideoList({Key? key, required this.videoInfo}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final formattedViewCount =
-        _ViewCountFormatter.format(videoInfo.streamNumber);
+    final formattedViewCount = formatViewCount(videoInfo.streamNumber);
 
     return Column(
       children: <Widget>[
@@ -243,12 +265,12 @@ class _VideoList extends StatelessWidget {
               videoInfo.imageUrl,
             ),
             Padding(
-              padding: const EdgeInsets.all(8.0),
+              padding: const EdgeInsets.all(8),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.end,
                 children: <Widget>[
-                  const Icon(Icons.equalizer, color: Colors.white, size: 24.0),
-                  const SizedBox(width: 8.0),
+                  const Icon(Icons.equalizer, color: Colors.white, size: 24),
+                  const SizedBox(width: 8),
                   _VideoTime(time: videoInfo.videoTime),
                 ],
               ),
@@ -256,7 +278,7 @@ class _VideoList extends StatelessWidget {
           ],
         ),
         Container(
-          padding: const EdgeInsets.all(8.0),
+          padding: const EdgeInsets.all(8),
           height: 76,
           color: Colors.grey[850], // _CustomColors.darkGreyの代わり
           child: Row(
@@ -310,41 +332,38 @@ class _VideoList extends StatelessWidget {
   }
 }
 
-class _ViewCountFormatter {
-  static String format(int viewCount) {
-    if (viewCount < 10000) {
-      // 1万未満の場合はそのまま表示
-      return NumberFormat("#,###").format(viewCount);
-    } else if (viewCount < 100000) {
-      // 1万以上10万未満の場合は、小数点第一位までの万単位で表示
-      double inTenThousands = viewCount / 10000;
-      return '${inTenThousands.toStringAsFixed(1)}万';
-    } else {
-      // 10万以上の場合は、整数の万単位で表示
-      int inTenThousands = (viewCount / 10000).floor();
-      return '$inTenThousands万';
-    }
+String formatViewCount(int viewCount) {
+  if (viewCount < 10000) {
+    // 1万未満の場合はそのまま表示
+    return NumberFormat('#,###').format(viewCount);
+  } else if (viewCount < 100000) {
+    // 1万以上10万未満の場合は、小数点第一位までの万単位で表示
+    final inTenThousands = viewCount / 10000;
+    return '${inTenThousands.toStringAsFixed(1)}万';
+  } else {
+    // 10万以上の場合は、整数の万単位で表示
+    final inTenThousands = (viewCount / 10000).floor();
+    return '$inTenThousands万';
   }
 }
 
 class _VideoTime extends StatelessWidget {
+  const _VideoTime({required this.time});
   final String time;
-
-  const _VideoTime({Key? key, required this.time}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.symmetric(horizontal: 2.0, vertical: 1.0),
+      padding: const EdgeInsets.symmetric(horizontal: 2, vertical: 1),
       decoration: BoxDecoration(
         color: Colors.black,
-        borderRadius: BorderRadius.circular(4.0),
+        borderRadius: BorderRadius.circular(4),
       ),
       child: Text(
         time,
         style: const TextStyle(
           color: Colors.white,
-          fontSize: 16.0,
+          fontSize: 16,
         ),
       ),
     );
@@ -352,8 +371,8 @@ class _VideoTime extends StatelessWidget {
 }
 
 class _VideoProfileIcon extends StatelessWidget {
+  const _VideoProfileIcon({required this.url});
   final String url;
-  const _VideoProfileIcon({Key? key, required this.url}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -379,24 +398,36 @@ class _CustomBottomNavigationBar extends StatelessWidget {
   Widget build(BuildContext context) {
     return BottomNavigationBar(
       items: [
-        _BottomNavItemFactory.createItem(
-            iconData: Icons.home_outlined, label: 'ホーム'),
-        _BottomNavItemFactory.createItem(iconData: Icons.explore, label: '検索'),
-        _BottomNavItemFactory.createItem(
-            iconData: Icons.add_circle_outline, label: '', iconSize: 40),
-        _BottomNavItemFactory.createItem(
-            iconData: Icons.slow_motion_video_outlined, label: '登録チャンネル'),
-        _BottomNavItemFactory.createItem(
-            iconData: Icons.smart_display_outlined, label: 'ライブラリ'),
+        createBottomNavItem(
+          iconData: Icons.home_outlined,
+          label: 'ホーム',
+        ),
+        createBottomNavItem(
+          iconData: Icons.explore,
+          label: '検索',
+        ),
+        createBottomNavItem(
+          iconData: Icons.add_circle_outline,
+          label: '',
+          iconSize: 40,
+        ),
+        createBottomNavItem(
+          iconData: Icons.slow_motion_video_outlined,
+          label: '登録チャンネル',
+        ),
+        createBottomNavItem(
+          iconData: Icons.smart_display_outlined,
+          label: 'ライブラリ',
+        ),
       ],
       unselectedItemColor: Colors.white,
       type: BottomNavigationBarType.fixed,
       selectedLabelStyle: const TextStyle(
-        fontSize: 10.0,
+        fontSize: 10,
         fontWeight: FontWeight.bold,
       ),
       unselectedLabelStyle: const TextStyle(
-        fontSize: 10.0,
+        fontSize: 10,
         fontWeight: FontWeight.bold,
       ),
     );
@@ -404,14 +435,12 @@ class _CustomBottomNavigationBar extends StatelessWidget {
 }
 
 class _CustomIconButton extends StatelessWidget {
-  final IconData iconData;
-  final VoidCallback onPressed;
-
   const _CustomIconButton({
-    Key? key,
     required this.iconData,
     required this.onPressed,
-  }) : super(key: key);
+  });
+  final IconData iconData;
+  final VoidCallback onPressed;
 
   @override
   Widget build(BuildContext context) {
@@ -422,15 +451,13 @@ class _CustomIconButton extends StatelessWidget {
   }
 }
 
-class _BottomNavItemFactory {
-  static BottomNavigationBarItem createItem({
-    required IconData iconData,
-    required String label,
-    double iconSize = 24.0,
-  }) {
-    return BottomNavigationBarItem(
-      icon: Icon(iconData, size: iconSize),
-      label: label,
-    );
-  }
+BottomNavigationBarItem createBottomNavItem({
+  required IconData iconData,
+  required String label,
+  double iconSize = 24.0,
+}) {
+  return BottomNavigationBarItem(
+    icon: Icon(iconData, size: iconSize),
+    label: label,
+  );
 }
